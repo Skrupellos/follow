788f250f83c9310fca63c388e30b90414f9c33a8
Skrupellos <skruppy@onmars.eu>
2016-06-30 02:14:12 +0200

    [graph] Added reachability functions

--- src/main/java/com/github/skrupellos/follow/graph/GraphNode.java
+++ src/main/java/com/github/skrupellos/follow/graph/GraphNode.java
@@ -1,6 +1,9 @@
 package com.github.skrupellos.follow.graph;
 
 import java.lang.Iterable;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Stack;
 
 
 
@@ -63,5 +66,46 @@ public abstract class GraphNode<
 		heads.takeover(node.heads());
 		
 		return self;
+	}
+	
+	
+	public Collection<? extends GraphNode<NODE, ARROW>> reachable() {
+		return reachable(null);
+	}
+	
+	
+	public boolean isReachable(NODE needle) {
+		return reachable(needle) == null;
+	}
+	
+	
+	private Collection<? extends GraphNode<NODE, ARROW>> reachable(NODE needle) {
+		Collection<GraphNode<NODE, ARROW>> seen = new HashSet<GraphNode<NODE, ARROW>>();
+		
+		// Using a stack results in a depth-first search, which should answer
+		// queries for the end state faster.
+		Stack<GraphNode<NODE, ARROW>> open = new Stack<GraphNode<NODE, ARROW>>();
+		
+		seen.add(this);
+		
+		for(GraphNode<NODE, ARROW> pos = this; pos != null; pos = open.pop()) {
+			for(ARROW arrow : pos.tails()) {
+				GraphNode<NODE, ARROW> next = arrow.head();
+				
+				// If this is a node, which has never been seen bevore, add it
+				// to the list of open nodes and the set of seen nodes. Since
+				// seen is a set, there won't be any two entries for the same
+				// node.
+				if(seen.add(next)) {
+					if(next == needle) {
+						return null;
+					}
+					
+					open.push(next);
+				}
+			}
+		}
+		
+		return seen;
 	}
 }
