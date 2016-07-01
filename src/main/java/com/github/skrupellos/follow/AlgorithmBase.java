2c669e6575cd8401baae61dd940ba39d715a0a37
Skrupellos <skruppy@onmars.eu>
2016-07-01 17:09:56 +0200

    [follow] Split AlgorithmBase in AlgorithmBase and MapHelper

--- src/main/java/com/github/skrupellos/follow/AlgorithmBase.java
+++ src/main/java/com/github/skrupellos/follow/AlgorithmBase.java
@@ -4,8 +4,7 @@ import java.util.Map;
 import java.util.HashMap;
 
 
-public class AlgorithmBase<KEY, VALUE> {
-	private final Map<KEY, VALUE> map = new HashMap<KEY, VALUE>();
+public class AlgorithmBase<KEY, VALUE> extends MapHelper<KEY, VALUE> {
 	private final KEY root;
 	
 	
@@ -15,37 +14,6 @@ public class AlgorithmBase<KEY, VALUE> {
 	
 	
 	public VALUE result() {
-		VALUE result = map.get(root);
-		assert(result != null);
-		return result;
-	}
-	
-	
-	protected void define(KEY key, VALUE value) {
-		if(key == null) {
-			throw new IllegalArgumentException("null");
-		}
-		if(value == null) {
-			throw new IllegalArgumentException("null");
-		}
-		
-		VALUE ret = map.putIfAbsent(key, value);
-		if(ret != null) {
-			throw new IllegalArgumentException("Not a new regex");
-		}
-	}
-	
-	
-	protected VALUE lookup(KEY key) {
-		if(key == null) {
-			throw new IllegalArgumentException("null");
-		}
-		
-		VALUE value = map.get(key);
-		if(value == null) {
-			throw new IllegalArgumentException("regex does not exist");
-		}
-		
-		return value;
+		return lookup(root);
 	}
 }
