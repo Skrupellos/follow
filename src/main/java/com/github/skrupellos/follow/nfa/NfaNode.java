8f8ab62ca15e2498e4410014ae3aed313f9829dd
Skrupellos <skruppy@onmars.eu>
2016-07-02 20:07:54 +0200

    [nfa] Extend NfaNodes not save weather they are are final states

--- src/main/java/com/github/skrupellos/follow/nfa/NfaNode.java
+++ src/main/java/com/github/skrupellos/follow/nfa/NfaNode.java
@@ -4,13 +4,28 @@ import com.github.skrupellos.follow.graph.GraphNode;
 
 
 public class NfaNode<T> extends GraphNode<NfaNode<T>, NfaArrow<T>> {
+	public boolean accepting = false;
+	
+	
 	public NfaNode() {
+		this(false);
+	}
+	
+	
+	public NfaNode(boolean accepting) {
 		super();
+		this.accepting = accepting;
 	}
 	
 	
 	public NfaNode(Iterable<NfaArrow<T>> tails, Iterable<NfaArrow<T>> heads) {
+		this(tails, heads, false);
+	}
+	
+	
+	public NfaNode(Iterable<NfaArrow<T>> tails, Iterable<NfaArrow<T>> heads, boolean accepting) {
 		super(tails, heads);
+		this.accepting = accepting;
 	}
 	
 	
