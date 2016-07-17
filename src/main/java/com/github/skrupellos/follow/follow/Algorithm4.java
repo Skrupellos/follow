e7e59b787f78c0d7ae55eebe8b8798e7c20a5a5c
Skrupellos <skruppy@onmars.eu>
2016-07-18 00:28:52 +0200

    Added License header in each source file

--- src/main/java/com/github/skrupellos/follow/follow/Algorithm4.java
+++ src/main/java/com/github/skrupellos/follow/follow/Algorithm4.java
@@ -1,3 +1,20 @@
+/* This file is part of Follow (https://github.com/Skrupellos/follow).
+ * Copyright (c) 2016 Skruppy <skruppy@onmars.eu> and kratl.
+ *
+ * Follow is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Follow is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Follow. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.github.skrupellos.follow.follow;
 
 import java.util.HashSet;
